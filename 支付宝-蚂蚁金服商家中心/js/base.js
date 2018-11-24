be90f3e29bfa2b6fb47e3a2412db418cc757c98b
luguanghua <253509785@qq.com>
2018-11-24 11:06:54 +0800

    最后修改,周总牛逼

--- 支付宝-蚂蚁金服商家中心/js/base.js
+++
@@ -1,7 +0,0 @@
-window.lst = {};
-
-lst.transitionend = function (obj,callback) {
-    obj.addEventListener('transitionend',callback);
-    obj.addEventListener('webkitTransitionEnd',callback);
-
-};
